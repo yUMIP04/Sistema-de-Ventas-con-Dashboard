69b10e3aff285c801bfebd4b706a8155f5cdf95e
yUMIP04 <yumip5719@gmail.com>
2026-05-19 19:05:01 -0600

    aux_pandas: -Revisa que el archivo no este vacio -Revisa que las columnas obligatorias esten en el archivo

--- aux_pandas.py
+++ aux_pandas.py
@@ -5,6 +5,29 @@ archivo_csv = 'static/uploads/CSV/archivo.csv'
 
 def ProcesamientoDatos_CSV(archivo_csv):
 
-    #🌟QUE NO SEAN ARCHIVOS VACIOS
+  archivo_pandas = pd.read_csv(archivo_csv)
 
-    
+#🌟Revisar que el archivo no este vacio
+  if archivo_pandas.empty:
+    print("❌ El archivo esta vacio o no tiene Datos.")
+
+  else:
+    print("🥳 El archivo contiene Datos.")
+
+    verificar_Archivo = archivo_pandas.columns
+
+    columnas_obligatorias = ['Nombre Producto',  'Categoria', 'Precio', 'Cantidad Vendida', 'Fecha']
+
+    for columna in columnas_obligatorias:
+      
+      if columna in verificar_Archivo:
+
+        print("🥳Las columnas se encuentran en este archivo")
+
+      else: 
+
+        print("❌Las o alguna columna no se encuentra en el archivo")
+
+    #🌟Que el archivo tenga las columnas que debe traer
+
+ProcesamientoDatos_CSV(archivo_csv)
