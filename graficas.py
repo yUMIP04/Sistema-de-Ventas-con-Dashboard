3c393ee44a14f4a33fd033b1356bb20d34669e6f
yUMIP04 <yumip5719@gmail.com>
2026-05-26 18:51:46 -0600

    -Grafica de barras hecha, -Se depuraron los valores del archivo CSV para las graficas

--- graficas.py
+++ graficas.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import plotly.express as px
 import os
+import datetime
 
 
 def Create_Graficas (archivo_csv):
@@ -14,21 +15,47 @@ def Create_Graficas (archivo_csv):
     else:
 
         datos_csv = pd.read_csv(ruta)
+        print(datos_csv)
         print("🥳 Si existe el archivo")
 
         #ventas por producto
 
-        ventas = datos_csv["Cantidad Vendida"].str.replace("vendidos", "").str.replace("vendidas", "").str.replace("productos", "").str.replace("cantidad", "")
+    ventas = datos_csv["Cantidad Vendida"].str.replace("vendidos", "").str.replace("vendidas", "").str.replace("productos", "").str.replace("cantidad", "")
 
-        ventas_int = ventas.astype('int')
-        datos_csv["Cantidad Vendida"] = ventas_int
+    ventas_int = ventas.astype('int')
+    datos_csv["Cantidad Vendida"] = ventas_int
         
-        ventas_producto = {
+    ventas_producto = {
             "Nombre Producto" : datos_csv["Nombre Producto"],
             "Cantidad Vendida": datos_csv["Cantidad Vendida"]
         }
 
-        Tabla_ventasProducto = pd.DataFrame(ventas_producto)
+    Tabla_ventasProducto = pd.DataFrame(ventas_producto)
 
-        print(Tabla_ventasProducto)
+    try:
+        Grafica_Barras = px.bar(Tabla_ventasProducto, x="Nombre Producto", y="Cantidad Vendida", title="Ventas por producto")
+        #Grafica_Barras.show()
+
+        #return Grafica_Barras
+    
+    except Exception as e:
+        print(f"Hubo un error al Generar la grafica de barras: {e}")
+
+    #ventas por fecha
+    
+    Precios_float = datos_csv["Precio"].str.replace("$", "").astype('float')
+    datos_csv["Precio"] = Precios_float
+    fecha_formateada = pd.to_datetime(datos_csv["Fecha"], format='%d/%m/%y', errors='cocerce')
+    datos_csv["Fecha"] = fecha_formateada
+
+    agrupando = datos_csv.groupby('')
+    ventas_fecha = {
+        "Nombre Producto": datos_csv["Nombre Producto"],
+        "Precio":datos_csv["Precio"],
+        "Fecha": datos_csv["Fecha"]
+    }
+
+    TablaVentas_fecha = pd.DataFrame(ventas_fecha)
+    
+    
 Create_Graficas("archivo.csv")
