6c4cf02e358cf731e255858d21527290db1540e4
yUMIP04 <yumip5719@gmail.com>
2026-05-30 15:00:05 -0600

    limpieza de datos en aux_pandas.py

--- aux_pandas.py
+++ aux_pandas.py
@@ -1,75 +1,33 @@
 import pandas as pd
 import os
+import datetime
 
 def ProcesamientoDatos_CSV(archivo_csv):
+    ruta_csv = f'static/uploads/CSV/{archivo_csv}'
 
-  archivo_csv = f'static/uploads/CSV/{archivo_csv}'
-  archivo_pandas = pd.read_csv(archivo_csv)
+    try:
+        if os.path.exists(ruta_csv):
+            print("👌Se encontro con exito el archivo")
+            
+            #🌟limpieza datos
 
-#🌟Revisar que el archivo no este vacio
-  if archivo_pandas.empty:
-    print("❌ El archivo esta vacio o no tiene Datos.")
+            csv = pd.read_csv(ruta_csv)
 
-  else:
-    print("🥳 El archivo contiene Datos.")
+            precio_formateado = csv["Precio"].str.lower().str.replace("$", "").str.replace("pesos", "").astype('float')
+            cantidad_formateada = csv["Cantidad Vendida"].str.lower().str.replace("vendidos", "").str.replace("vendidas", "").str.replace("vendido", "").str.replace("vendida", "").astype('int')
+            fecha_formateada = pd.to_datetime(csv["Fecha"], format='%d/%m/%Y', errors='raise')
 
-     #🌟Que el archivo tenga las columnas que debe traer
-  verificar_Archivo = archivo_pandas.columns
-
-  columnas_obligatorias = ['Nombre Producto',  'Categoria', 'Precio', 'Cantidad Vendida', 'Fecha']
-
-  for columna in columnas_obligatorias:
-      
-    if columna in verificar_Archivo:
-
-      print("🥳Las columnas se encuentran en este archivo")
-
-    else: 
-
-      print("❌Las o alguna columna no se encuentra en el archivo")
-
-        #🌟total ventas
-  pandas_precio = archivo_pandas["Precio"].str.replace("$", "")
-  pandas_ventas =archivo_pandas["Cantidad Vendida"].str.replace("vendidos", "").str.replace("vendidas", "")
-
-  precio_int = pandas_precio.astype('float')
-  ventas_int = pandas_ventas.astype('int')
-
-  Precio_Ventas = precio_int * ventas_int
-  
-  TotalMonetario_Ventas = Precio_Ventas.sum()
-
-  #🌟 total productos vendidos
-
-  TotalProduct_Vendidos = sum(ventas_int)
-
-  #🌟 promedio de ventas
-
-  Promedio_Ventas = precio_int.mean()
-  
-  #🌟 categoria con mas ingresos
-  archivo_pandas["Precio"] = pandas_precio.astype('float')
-
-  agrupando_categorias = archivo_pandas.groupby('Categoria')["Precio"].sum().sort_values(ascending=False)
-
-  #🌟 producto mas vendido
-  NombreProducto_tabla = archivo_pandas["Nombre Producto"]
-
-  datosMax ={
-    "Nombre Producto": NombreProducto_tabla,
-    "Cantidad Vendida": ventas_int
-  }
-
-  TablaProductos_Max = pd.DataFrame(datosMax)
-
-  ProductoMax_Vendido = TablaProductos_Max.sort_values(by='Cantidad Vendida', ascending=False)
-
-  print(ProductoMax_Vendido)
-
-  print(agrupando_categorias)
-  #🌟 salidas de la consola
-  print(f"Total de ventas:${TotalMonetario_Ventas}.")
-  print(f"Cifra de productos vendidos: {TotalProduct_Vendidos} piezas de toda la tienda.")
-  print(f"Promedio de ventas es: {Promedio_Ventas}")
+            csv["Precio"] = precio_formateado
+            csv["Cantidad Vendida"] = cantidad_formateada
+            csv["Fecha"] = fecha_formateada
+            print(csv)
+            
+        else:
+            print("❌ No se encontro ningun archivo csv.")
+            return None
+        
+    except Exception as e:
+        print(f"Hubo un error al encontrar el archivo: {e}")
+        return None
 
 ProcesamientoDatos_CSV("archivo.csv")
