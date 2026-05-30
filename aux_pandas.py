764a82a8e59fbc7ce40d96b894b7746345d51ebf
yUMIP04 <yumip5719@gmail.com>
2026-05-30 15:23:25 -0600

    sacando el total de ventas

--- aux_pandas.py
+++ aux_pandas.py
@@ -8,20 +8,39 @@ def ProcesamientoDatos_CSV(archivo_csv):
     try:
         if os.path.exists(ruta_csv):
             print("👌Se encontro con exito el archivo")
+
             
             #🌟limpieza datos
 
             csv = pd.read_csv(ruta_csv)
 
-            precio_formateado = csv["Precio"].str.lower().str.replace("$", "").str.replace("pesos", "").astype('float')
-            cantidad_formateada = csv["Cantidad Vendida"].str.lower().str.replace("vendidos", "").str.replace("vendidas", "").str.replace("vendido", "").str.replace("vendida", "").astype('int')
-            fecha_formateada = pd.to_datetime(csv["Fecha"], format='%d/%m/%Y', errors='raise')
+            if not csv.empty:
+                
+                print("👌El archivo tiene contenido")
 
-            csv["Precio"] = precio_formateado
-            csv["Cantidad Vendida"] = cantidad_formateada
-            csv["Fecha"] = fecha_formateada
-            print(csv)
-            
+                columnas_obligatorias = {"Nombre Producto", "Categoria", "Precio", "Cantidad Vendida", "Fecha"}
+                
+                revision_columnasObligatorias = columnas_obligatorias.issubset(csv.columns)
+
+                if revision_columnasObligatorias:
+                   
+                   print("👌 El archivo tiene las columnas obligatorias")
+
+                   precio_formateado = csv["Precio"].str.lower().str.replace("$", "").str.replace("pesos", "").astype('float')
+                   cantidad_formateada = csv["Cantidad Vendida"].str.lower().str.replace("vendidos", "").str.replace("vendidas", "").str.replace("vendido", "").str.replace("vendida", "").astype('int')
+                   fecha_formateada = pd.to_datetime(csv["Fecha"], format='%d/%m/%Y', errors='raise')
+
+                   csv["Precio"] = precio_formateado
+                   csv["Cantidad Vendida"] = cantidad_formateada
+                   csv["Fecha"] = fecha_formateada
+                   
+
+                   #Calculos
+
+                   csv["Total Ventas"] = csv["Cantidad Vendida"].mul(csv["Precio"])
+                   total_ventas_dinero = csv["Total Ventas"].sum()
+
+                   print(f"El total de ventas en dinero es $ {total_ventas_dinero } ")           
         else:
             print("❌ No se encontro ningun archivo csv.")
             return None
