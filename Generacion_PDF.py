fb83c1482cdc7a9adcab55f52a7d20ea42e646f8
yUMIP04 <yumip5719@gmail.com>
2026-06-10 19:33:42 -0600

    Creacion del membretado y de una carpeta especifica para estilos

--- Generacion_PDF.py
+++ Generacion_PDF.py
@@ -1,7 +1,10 @@
 from reportlab.lib.pagesizes import letter
 from reportlab.platypus import SimpleDocTemplate, Paragraph
+from reportlab.lib.styles import ParagraphStyle
 from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib import colors
 import os
+from StylesPDFS.Membretado import Titulo_Membretado
 
 def Generar_PDF(nombre_archivo):
 
@@ -16,8 +19,27 @@ def Generar_PDF(nombre_archivo):
 
         print("¡Si existe la ruta!")
         ruta_final = os.path.join(ruta, nombre_archivo)
-        print("Redirigiendo a la ruta: ", ruta_final)
 
-        
+        story = []
+
+        #Estilos
+
+        try:
+
+           doc = SimpleDocTemplate(ruta_final, pagesizes="letter", leftMargin=72, rightMargin=72, topMargin=142, bottomMargin=72)
+
+           Titulo_Resultados = Paragraph("<b>Resultados</b>")
+
+           story.append(Titulo_Resultados)
+
+           doc.build(story, onFirstPage=Titulo_Membretado, onLaterPages=Titulo_Membretado)
+
+           print("Creando el archivo pdf con exito")
+        except Exception as e:
+
+            print(f"Hubo un error al crear el archivo pdf : {e}")
+            return None
+
+
 
 Generar_PDF("Prueba.pdf")
---
+++ StylesPDFS/Membretado.py
@@ -0,0 +1,12 @@
+
+
+def Titulo_Membretado(canvas, doc):
+
+    canvas.saveState()
+
+    canvas.setFont("Helvetica-Bold", 16)
+    canvas.drawString(54,750, "Analisis de Ventas")
+
+    canvas.line(54, 742, 558, 742)
+
+    canvas.restoreState()
