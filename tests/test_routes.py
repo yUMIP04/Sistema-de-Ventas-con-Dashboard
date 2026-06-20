92fa2a2fbe2887c3bad6ceca875b8fc362423924
yUMIP04 <yumip5719@gmail.com>
2026-06-20 17:49:30 -0600

    Prueba unitaria con el middleware

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,6 +1,10 @@
 import pytest
 from app import app
+import os
+import jwt
+from dotenv import load_dotenv
 
+load_dotenv()
 @pytest.fixture
 
 def cliente():
@@ -15,6 +19,9 @@ def cliente():
 
 def test_pagina_inicio_carga_correctamente(cliente):
 
+    with cliente.session_transaction() as sesion_falsa:
+        sesion_falsa['nombre_usuario'] = 'victoria'
+
     respuesta = cliente.get('/Inicio')
 
     assert respuesta.status_code == 200
@@ -24,13 +31,19 @@ def test_pagina_inicio_carga_correctamente(cliente):
 
 def test_generar_pdf(cliente):
 
+    llave_secreta = os.getenv("SECRET_KEYJWT", "ProbandoJWT2026")
+    token_prueba = jwt.encode({"usuario_id": 1, "Nombre_Usuario": "victoria"}, llave_secreta, algorithm="HS256")
+
+
     with cliente.session_transaction() as sesion_falsa:
+
+        sesion_falsa['nombre_usuario'] = 'victoria'
 
         sesion_falsa['archivo_actual'] = 'ventas_juguete.csv'
         sesion_falsa['total_ventas_dinero'] = 2500.50
         sesion_falsa['total_productos_vendidos'] = 14
         sesion_falsa['ticket_promedio'] = 178.60
 
-        respuesta = cliente.post('/Inicio', data={'generar-pdf': 'true'})
+    respuesta = cliente.post('/Inicio', data={'generar-pdf': 'true'})
 
-        assert respuesta.status_code == 200
+    assert respuesta.status_code == 200
